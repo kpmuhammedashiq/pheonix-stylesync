3f09e8a5da51762fde4ea45f455ff44ec993830f
kpmuhammedashiq <kpmuhammedashiq@gmail.com>
2025-04-12 22:12:21 +0530

    rerntal and inventory service initial setup

---
+++ rental-service/src/main/java/com/apparel/rentalservice/RentalServiceApplication.java
@@ -0,0 +1,12 @@
+package com.apparel.rentalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalServiceApplication.class, args);
+    }
+}
