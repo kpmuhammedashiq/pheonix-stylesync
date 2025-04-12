a2d3bbf1ef583b9d92d031878fa939b4449b6867
kpmuhammedashiq <kpmuhammedashiq@gmail.com>
2025-04-12 23:20:59 +0530

    Add User Service and configure WebClient for Rental Service

---
+++ user-service/src/main/java/com/stylesync/user/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.stylesync.user.controller;
+
+import com.stylesync.user.model.User;
+import com.stylesync.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAll() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User save(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/{username}")
+    public User getByUsername(@PathVariable String username) {
+        return userService.getUserByUsername(username);
+    }
+}
---
+++ user-service/src/main/java/com/stylesync/user/model/User.java
@@ -0,0 +1,17 @@
+package com.stylesync.user.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "app_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String role;
+}
